2c74006dc48019992dc05dca54221f57680966dd
nourinalam <nourinalam26@gmail.com>
2022-11-14 13:28:51 +0700

    membuat project idcard

---
+++ src/main/java/idcard/pws/idcard/IdcardApplication.java
@@ -0,0 +1,13 @@
+package idcard.pws.idcard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IdcardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IdcardApplication.class, args);
+	}
+
+}
---
+++ src/test/java/idcard/pws/idcard/IdcardApplicationTests.java
@@ -0,0 +1,13 @@
+package idcard.pws.idcard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IdcardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
